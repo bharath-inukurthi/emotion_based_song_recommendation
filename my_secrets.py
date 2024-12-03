722354f059943a173303ee44528bdec9a63dce3a
bharath kumar inukurthi <163440012+bharath-inukurthi@users.noreply.github.com>
2024-12-03 22:42:23 +0530

    Add files via upload
    fixed stratup bug

---
+++ my_secrets.py
@@ -0,0 +1,10 @@
+
+
+sheet_id=" "
+SPOTIFY_CLIENT_ID = 'your spotify ID'
+SPOTIFY_CLIENT_SECRET = 'your spotify secret'
+
+#SCOPE varaible should be according to what functionality you want to achieve with spotify api
+# for designed functionality the declared SCOPE should be sufficient
+
+SCOPE = "user-read-playback-state user-modify-playback-state"
